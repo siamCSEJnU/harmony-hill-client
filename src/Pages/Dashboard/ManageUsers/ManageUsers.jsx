ca501df95347dd807a8e1dc1bfd7e5f4ef476529
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-06-11 03:08:04 +0600

    query client provided to the app

--- src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,12 +1,19 @@
 import { Helmet } from "react-helmet-async";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const ManageUsers = () => {
+  const [axiosSecure] = useAxiosSecure();
+  const { data: users = [], refetch } = useQuery(["users"], async () => {
+    const res = await axiosSecure.get("/users");
+    return res.data;
+  });
   return (
     <div>
       <Helmet>
         <title>Harmony Hill | Manage Users</title>
       </Helmet>
-      <h2>manage users</h2>
+      <h2>manage users : {users.length}</h2>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,18 @@ import AuthProvider from "./Providers/AuthProvider/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
         {" "}
-        <RouterProvider router={router}></RouterProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}></RouterProvider>
+        </QueryClientProvider>
       </HelmetProvider>
       <Toaster />
       <ToastContainer></ToastContainer>
