26fc749dc593afcda5ca5e6b45537a3b19ca331c
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-06-12 05:51:28 +0600

    class selection is done

--- src/Pages/AllClasses/ClassItem.jsx
+++ src/Pages/AllClasses/ClassItem.jsx
@@ -1,9 +1,51 @@
-import { NavLink } from "react-router-dom";
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useState } from "react";
+import { ImSpinner4 } from "react-icons/im";
 
 const ClassItem = ({ item }) => {
-  console.log(item);
+  const { user } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+  const [loading, setLoading] = useState(false);
+
+  const handleSelectClass = (item) => {
+    if (!user) {
+      return Swal.fire("Before selecting login please  !!");
+    }
+    setLoading(true);
+
+    const { email, image, name, price, status, available } = item;
+    const selectedClass = {
+      name: item.class,
+      instructor: name,
+      image,
+      email,
+      price,
+      status,
+      available,
+    };
+    console.log(selectedClass);
+    axiosSecure.post("/selectClass", selectedClass).then((response) => {
+      if (response.data.insertedId) {
+        setLoading(false);
+        Swal.fire({
+          position: "top",
+          icon: "success",
+          title: "Class has been selected successfuly",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+
   return (
-    <div className="bg-blue-100 p-3  rounded-md  ">
+    <div
+      className={` p-3  rounded-md ${
+        item?.available == 0 ? "bg-red-400" : "bg-blue-100"
+      }`}
+    >
       <img
         src={item.image}
         alt="instructorPhoto"
@@ -11,7 +53,9 @@ const ClassItem = ({ item }) => {
       />
       <div className=" font-semibold text-lg ">
         <div className="h-28 overflow-hidden space-y-1   ">
-          <h3 className="text-3xl font-bold text-center ">{item.class}</h3>
+          <h3 className="text-3xl font-bold text-center text-blue-500 ">
+            {item.class}
+          </h3>
           <p className="text-center font-bold ">Instructor : {item.name}</p>
         </div>
         <div className="h-16 mt-1 overflow-hidden space-y-1 ">
@@ -19,8 +63,25 @@ const ClassItem = ({ item }) => {
           <p>Price : ${item.price}</p>
         </div>
         <div className="text-center mt-2 mb-1 ">
-          <button className="btn w-full bg-emerald-400 outline-0 border-emerald-400 ">
-            Select Class
+          <button
+            disabled={
+              item?.available == 0 ||
+              user?.role == "admin" ||
+              user?.role == "instructor"
+                ? true
+                : false
+            }
+            onClick={() => handleSelectClass(item)}
+            className="btn w-full bg-emerald-400 outline-0 border-emerald-400 "
+          >
+            {loading ? (
+              <ImSpinner4
+                className="animate-spin m-auto text-blue-800"
+                size={32}
+              ></ImSpinner4>
+            ) : (
+              "Select Class"
+            )}
           </button>
         </div>
       </div>
--- src/Pages/Instructors/ClassCard.jsx
+++ src/Pages/Instructors/ClassCard.jsx
@@ -4,7 +4,9 @@ const ClassCard = ({ item }) => {
     <div className="bg-blue-100 p-3 rounded-md space-y-3 ">
       <img src={item.image} alt="classPhoto" className="w-96 rounded-md  " />
       <div className="space-y-2 font-semibold text-lg ">
-        <h3 className="text-3xl font-bold text-center mb-5">{item.class}</h3>
+        <h3 className="text-3xl font-bold text-center text-blue-500 mb-5">
+          {item.class}
+        </h3>
         <p>Price : ${item.price}</p>
         <p>Available Seats : {item.available}</p>
       </div>
