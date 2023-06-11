31dda899290c8977789f5e22806aab7240572d1e
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-06-11 08:08:55 +0600

    add class functionality done

--- src/Pages/Dashboard/AddClasses/AddClasses.jsx
+++ src/Pages/Dashboard/AddClasses/AddClasses.jsx
@@ -2,17 +2,73 @@ import { Helmet } from "react-helmet-async";
 import SectionTItle from "../../../Components/Shared/SectionTitle/SectionTItle";
 import { useForm } from "react-hook-form";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+
+const image_hosting_token = import.meta.env.VITE_IMAGE_UPLOAD_TOKEN;
 
 const AddClasses = () => {
   const { user } = useAuth();
-  const { register } = useForm();
+  const [axiosSecure] = useAxiosSecure();
+
+  const image_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_token}`;
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const handleAddClass = (data) => {
+    const formData = new FormData();
+    formData.append("image", data.image[0]);
+    fetch(image_hosting_url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imageResponse) => {
+        if (imageResponse.success) {
+          const imgURL = imageResponse.data.display_url;
+          const {
+            className,
+            instructorName,
+            instructorEmail,
+            available,
+            price,
+          } = data;
+
+          const newClass = {
+            class: className,
+            name: instructorName,
+            email: instructorEmail,
+            image: imgURL,
+            available,
+            price: parseFloat(price),
+            status: "pending",
+          };
+          axiosSecure.post("/class", newClass).then((response) => {
+            if (response.data.insertedId) {
+              reset();
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "New Class has been added successfuly",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+          });
+        }
+      });
+  };
+
   return (
     <div>
       <Helmet>
         <title>Harmony Hill | Add Classes</title>
       </Helmet>
-      <SectionTItle heading={"Add A Class"}></SectionTItle>
-      <form className=" w-3/5 mx-auto space-y-3 bg-sky-400  rounded-md p-5">
+      <SectionTItle heading={"Add A New Class"}></SectionTItle>
+      <form
+        onSubmit={handleSubmit(handleAddClass)}
+        className=" w-3/5 mx-auto space-y-3 bg-sky-400  rounded-md p-5"
+      >
         <div className="flex gap-10 items-center justify-between">
           <div className="space-y-2 w-1/2">
             <label
@@ -107,8 +163,12 @@ const AddClasses = () => {
             />
           </div>
         </div>
-        <div className="py-4">
-          <input type="submit" value="ADD" className=" btn w-full p " />
+        <div className="py-4 ">
+          <input
+            type="submit"
+            value="ADD CLASS"
+            className=" btn w-full bg-emerald-400 focus:outline-0 border-emerald-400  "
+          />
         </div>
       </form>
     </div>
